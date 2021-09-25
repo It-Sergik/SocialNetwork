6815e04a6e3f4a087021f3f41f9869fb9ace75b5
It-Sergik <orlovergei@gmail.com>
2021-09-25 16:49:55 +0700

    Split into components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+      <nav className={"nav"}>
+          <div>
+              Profile
+          </div>
+          <div>
+              Message
+          </div>
+      </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+  return (
+      <div className={"content"}>
+          <img src={"https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"}/>
+          <div>
+              avatar + description
+          </div>
+          <div>
+              My Post
+              <div>
+                  New Post
+              </div>
+              <div>
+                  Post 1
+              </div>
+              <div>
+                  Post 2
+              </div>
+          </div>
+      </div>
+  );
+}
+
+export default Profile;
