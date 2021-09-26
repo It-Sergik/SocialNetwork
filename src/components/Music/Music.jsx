d34cffc29437b10765af930d821221532e27a84c
It-Sergik <orlovergei@gmail.com>
2021-09-26 19:04:35 +0700

    Created news, music, settings pages and configured links to them.

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,11 @@
+import styleMusic from './Music.module.css';
+
+const Music = () => {
+    return (
+        <div>
+            Music
+        </div>
+    )
+}
+
+export default Music;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,19 @@ const Navbar = () => {
   return (
       <nav className={styleNav.nav}>
           <div className={styleNav.item}>
-             <a>Profile</a>
+             <a href={"/profile"}>Profile</a>
           </div>
           <div className={styleNav.item}>
-              <a>Message</a>
+              <a href={"/dialogs"}>Message</a>
           </div>
           <div className={styleNav.item}>
-              <a>News</a>
+              <a href={"/news"}>News</a>
           </div>
           <div className={styleNav.item}>
-              <a>Music</a>
+              <a href={"/music"}>Music</a>
           </div>
           <div className={styleNav.item}>
-              <a>Settings</a>
+              <a href={"/settings"}>Settings</a>
           </div>
       </nav>
   );
